b7df442061d90f3bcc47617c79e091aa3661fda1
ilonatommy <ili.ili@interia.pl>
2020-07-29 16:13:28 +0200

    Multithreading with API.

--- video_service/main.py
+++ video_service/main.py
@@ -1,29 +1,17 @@
 import time
-from PIL import Image
-import numpy as np
-from cv2 import cv2
-from flask import Flask, request, Response
-import qi
-from multiprocessing import Process
+from flask import Flask
 from videoWrapper import VideoWrapper
-
-
 
 app = Flask('video_service')
 
 videoWrapper = VideoWrapper()
 
 
-def capture():
-    print("Capturing started")
-    while True:
-	a = 10
-
 @app.route('/video', methods=['GET'])
 def watch():
     try:
-	"""
-	# returns last 10 frames
+        """
+    # returns last 10 frames
         global video_service
         resolution = 2 	# VGA
         colorSpace = 11	# RGB
@@ -36,12 +24,12 @@ def watch():
         width = result[0]
         height = result[1]
         image = np.zeros((height, width, 3), np.uint8)
-	im = Image.frombytes("RGB", (width, height), image)
-	im.show()
-	iteration = 0
+    im = Image.frombytes("RGB", (width, height), image)
+    im.show()
+    iteration = 0
 
         while iteration < 10:
-	    iteration=iteration+1
+        iteration=iteration+1
             # get image
             result = video_service.getImageRemote(videoClient)
 
@@ -50,41 +38,38 @@ def watch():
             elif result[6] == None:
                 print 'no image data string.'
             else:
-		image = str(bytearray(result[6]))
-		im = Image.frombytes("RGB", (width, height), image)
+        image = str(bytearray(result[6]))
+        im = Image.frombytes("RGB", (width, height), image)
 
-    		im.show()
+        im.show()
 
         video_service.unsubscribe(videoClient)
-	"""
+        """
         return "Success"
     except Exception as ex:
         return str(ex)
+
 
 @app.route('/start', methods=['POST'])
 def startwatch():
     try:
-	# videoWrapper.client = videoWrapper.video_service.subscribe(
-	#	"python_client", 
-	#	videoWrapper.resolution, 
-	#	videoWrapper.colorSpace, 
-	#	videoWrapper.fps)
-	videoWrapper.captureFrames = True
-	videoWrapper.process.start()
-	return "Success"
-    except Exception as ex:
-        return str(ex)
-	
+        videoWrapper.stopThread()
+    finally:
+        try:
+            videoWrapper.startThread()
+            return "Success"
+        except Exception as ex:
+            return str(ex)
+
 
 @app.route('/stop', methods=['POST'])
 def stopwatch():
     try:
-	# videoWrapper.client.unsubscribe(videoClient)
-	videoWrapper.captureFrames = False
-	videoWrapper.process.join()
+        videoWrapper.stopThread()
         return "Success"
     except Exception as ex:
         return str(ex)
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=9000, use_reloader=False)
--- video_service/videoWrapper.py
+++ video_service/videoWrapper.py
@@ -1,39 +1,42 @@
-import time
-from PIL import Image
-import numpy as np
-from cv2 import cv2
-from flask import Flask, request, Response
 import qi
 from multiprocessing import Process
+from threading import Thread
 
 
-class VideoWrapper():
+class VideoWrapper:
     def __init__(self, ip="192.168.1.123", port="9559", language="English"):
-    	# type: (str, str, str) -> None
-	self.session = qi.Session()
-	try:
+        # type: (str, str, str) -> None
+        self.session = qi.Session()
+        try:
             self.session.connect("tcp://" + ip + ":" + port)
             self.video_service = self.session.service("ALVideoDevice")
             print("Robot connected to VIDEO module.")
         except RuntimeError:
-             print("Can't connect to Pepper at ip \""
-          	+ ip + "\" on port " + port + ".\n"
-              	+ "Please check your script arguments. "
-              	+ "Run with -h option for help.")
+            print("Can't connect to Pepper at ip \""
+                  + ip + "\" on port " + port + ".\n"
+                  + "Please check your script arguments. "
+                  + "Run with -h option for help.")
 
         self.resolution = 2
         self.colorSpace = 11
         self.fps = 20
-	self.captureFrames = False
+        self.captureFrames = False
         self.client = None
-	self.process = Process(target=self.capture)
+        self.process = Process(target=self.capture)
+        self.thread = None
+
+    def startThread(self):
+        self.captureFrames = True
+        self.thread = Thread(target=self.capture)
+        self.thread.daemon = True
+        self.thread.start()
+
+    def stopThread(self):
+        self.captureFrames = False
+        self.thread.join()
 
     def capture(self):
-	print 'getting images in remote'
-	print self.captureFrames
-	while self.captureFrames:
-	    # result = self.video_service.getImageRemote(self.client)
-	    print("A")
-
-
-
+        print 'getting images in remote'
+        while self.captureFrames:
+            # result = self.video_service.getImageRemote(self.client)
+            print(self.captureFrames)
