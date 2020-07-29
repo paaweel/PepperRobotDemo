bdfc10bf4c0625fde4f54f2d1280c4e0567313ac
ilonatommy <ili.ili@interia.pl>
2020-07-29 16:36:19 +0200

    Code prepared for testing video on the robot.

--- video_service/main.py
+++ video_service/main.py
@@ -1,4 +1,3 @@
-import time
 from flask import Flask
 from videoWrapper import VideoWrapper
 
@@ -10,41 +9,7 @@ videoWrapper = VideoWrapper()
 @app.route('/video', methods=['GET'])
 def watch():
     try:
-        """
-    # returns last 10 frames
-        global video_service
-        resolution = 2 	# VGA
-        colorSpace = 11	# RGB
-        fps = 20
-        videoClient = video_service.subscribe("python_client", resolution, colorSpace, fps)
-        print 'getting images in remote'	
-        result = video_service.getImageRemote(videoClient)
-
-        # create image
-        width = result[0]
-        height = result[1]
-        image = np.zeros((height, width, 3), np.uint8)
-    im = Image.frombytes("RGB", (width, height), image)
-    im.show()
-    iteration = 0
-
-        while iteration < 10:
-        iteration=iteration+1
-            # get image
-            result = video_service.getImageRemote(videoClient)
-
-            if result == None:
-                print 'cannot capture.'
-            elif result[6] == None:
-                print 'no image data string.'
-            else:
-        image = str(bytearray(result[6]))
-        im = Image.frombytes("RGB", (width, height), image)
-
-        im.show()
-
-        video_service.unsubscribe(videoClient)
-        """
+        frames = videoWrapper.getLastFrames()
         return "Success"
     except Exception as ex:
         return str(ex)
--- video_service/videoWrapper.py
+++ video_service/videoWrapper.py
@@ -1,6 +1,9 @@
+import numpy as np
+from PIL import Image
 import qi
 from multiprocessing import Process
 from threading import Thread
+from collections import deque
 
 
 class VideoWrapper:
@@ -16,7 +19,6 @@ class VideoWrapper:
                   + ip + "\" on port " + port + ".\n"
                   + "Please check your script arguments. "
                   + "Run with -h option for help.")
-
         self.resolution = 2
         self.colorSpace = 11
         self.fps = 20
@@ -24,6 +26,7 @@ class VideoWrapper:
         self.client = None
         self.process = Process(target=self.capture)
         self.thread = None
+        self.lastFrames = deque([], maxlen=10)
 
     def startThread(self):
         self.captureFrames = True
@@ -37,6 +40,38 @@ class VideoWrapper:
 
     def capture(self):
         print 'getting images in remote'
+        self.client = self.video_service.subscribe(
+            "python_client",
+            self.resolution,
+            self.colorSpace,
+            self.fps)
+        result = self.video_service.getImageRemote(self.client)
+        if result is None:
+            print 'cannot capture.'
+            self.stopThread()
+        elif result[6] is None:
+            print 'no image data string.'
+            self.stopThread()
+        width = result[0]
+        height = result[1]
+        image = np.zeros((height, width, 3), np.uint8)
+        im = Image.frombytes("RGB", (width, height), image)
+        self.lastFrames.append(im)
+
         while self.captureFrames:
-            # result = self.video_service.getImageRemote(self.client)
-            print(self.captureFrames)
+            result = self.video_service.getImageRemote(self.client)
+            if result is None:
+                print 'cannot capture.'
+                self.stopThread()
+            elif result[6] is None:
+                print 'no image data string.'
+                self.stopThread()
+            else:
+                image = str(bytearray(result[6]))
+                im = Image.frombytes("RGB", (width, height), image)
+                self.lastFrames.append(im)
+        self.video_service.unsubscribe(self.client)
+
+    def getLastFrames(self, n=10):
+        # it's faster to index a list than a deque collection
+        return list(self.lastFrames)[0:n]
